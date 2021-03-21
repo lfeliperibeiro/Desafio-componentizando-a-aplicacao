246bda4c3c9c2e45b54cc567f172e591264cddd3
Felipe Ribeiro <piporibeiro@gmail.com>
2021-03-20 22:37:31 -0300

    refactor: separate itens in components

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import {  useState } from 'react';
+import { SideBar } from './components/SideBar';
+import {Content } from './components/Content';
+import './styles/global.scss';
+
+import './styles/sidebar.scss';
+import './styles/content.scss';
+
+export function App() {
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <SideBar setSelectedGenreId={setSelectedGenreId} selectedGenreId={selectedGenreId}/>
+
+      <Content selectedGenreId={selectedGenreId}/>
+
+    </div>
+  )
+}
---
+++ src/components/SideBar.tsx
@@ -0,0 +1,47 @@
+import {Button} from "./Button";
+import {useEffect, useState} from "react";
+import {api} from "../services/api";
+
+export interface GenreResponseProps {
+    id: number;
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    title: string;
+}
+
+
+interface SideBarProps {
+    selectedGenreId: number;
+    setSelectedGenreId: (id: number) => void;
+
+}
+
+export function SideBar({selectedGenreId, setSelectedGenreId}: SideBarProps ) {
+    const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+    useEffect(() => {
+        api.get<GenreResponseProps[]>('genres').then(response => {
+            setGenres(response.data);
+        });
+    }, []);
+
+    function handleClickButton(id: number) {
+        setSelectedGenreId(id);
+    }
+  return (
+      <nav className="sidebar">
+          <span>Watch<p>Me</p></span>
+
+          <div className="buttons-container">
+              {genres.map(genre => (
+                  <Button
+                      key={String(genre.id)}
+                      title={genre.title}
+                      iconName={genre.name}
+                      onClick={() => handleClickButton(genre.id)}
+                      selected={selectedGenreId === genre.id}
+                  />
+              ))}
+          </div>
+
+      </nav>
+  )
+}
